c80628fff36a8962172efe02361791f386f86a37
Quitch <Quitch@users.noreply.github.com>
2023-05-31 11:57:32 +0100

    Display direct lobby ID for copying
    We can't seem to copy to clipboard through Javascript in PA

---
+++ ui/mods/com.pa.quitch.copy-replay-id/replay_browser/copy.js
@@ -0,0 +1,38 @@
+var copyReplayIDButtonLoaded;
+
+if (!copyReplayIDButtonLoaded) {
+  copyReplayIDButtonLoaded = true;
+
+  function copyReplayIDButton() {
+    try {
+      $(".section_controls").append(
+        loadHtml(
+          "coui://ui/mods/com.pa.quitch.copy-replay-id/replay_browser/lobby_id.html"
+        )
+      );
+
+      model.gameId = ko.observable("");
+
+      var formattedId = function () {
+        if (!model.canViewReplay()) {
+          return;
+        }
+
+        var currentSelectedGame = model.currentSelectedGame();
+        var replayId = currentSelectedGame.host_id;
+        var requiredContent = currentSelectedGame.required_content
+          ? currentSelectedGame.required_content + ":"
+          : "";
+        return requiredContent + replayId;
+      };
+
+      model.currentSelectedGame.subscribe(function () {
+        model.gameId(formattedId());
+      });
+    } catch (e) {
+      console.error(e);
+      console.error(JSON.stringify(e));
+    }
+  }
+  copyReplayIDButton();
+}
