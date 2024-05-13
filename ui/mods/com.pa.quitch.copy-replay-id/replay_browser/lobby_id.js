418ce4f5e483d5e888bc93cf94300c6502333f18
Quitch <Quitch@users.noreply.github.com>
2024-05-13 20:51:01 +0100

    Reduce indentation

--- ui/mods/com.pa.quitch.copy-replay-id/replay_browser/lobby_id.js
+++ ui/mods/com.pa.quitch.copy-replay-id/replay_browser/lobby_id.js
@@ -1,38 +1,40 @@
 var copyReplayIDButtonLoaded;
 
-if (!copyReplayIDButtonLoaded) {
+function copyReplayIDButton() {
+  if (copyReplayIDButtonLoaded) {
+    return;
+  }
+
   copyReplayIDButtonLoaded = true;
 
-  function copyReplayIDButton() {
-    try {
-      model.gameId = ko.observable("");
+  try {
+    model.gameId = ko.observable("");
 
-      $(".section_controls").append(
-        loadHtml(
-          "coui://ui/mods/com.pa.quitch.copy-replay-id/replay_browser/lobby_id.html"
-        )
-      );
+    $(".section_controls").append(
+      loadHtml(
+        "coui://ui/mods/com.pa.quitch.copy-replay-id/replay_browser/lobby_id.html"
+      )
+    );
 
-      var formattedId = function () {
-        var selectedGame = model.currentSelectedGame();
-        var replayId = selectedGame.host_id;
-        var requiredContent = selectedGame.required_content
-          ? selectedGame.required_content + ":"
-          : "";
-        return requiredContent + replayId;
-      };
+    var formattedId = function () {
+      var selectedGame = model.currentSelectedGame();
+      var replayId = selectedGame.host_id;
+      var requiredContent = selectedGame.required_content
+        ? selectedGame.required_content + ":"
+        : "";
+      return requiredContent + replayId;
+    };
 
-      model.currentSelectedGame.subscribe(function () {
-        if (!model.canViewReplay() || !model.currentSelectedGame()) {
-          return;
-        }
+    model.currentSelectedGame.subscribe(function () {
+      if (!model.canViewReplay() || !model.currentSelectedGame()) {
+        return;
+      }
 
-        model.gameId(formattedId());
-      });
-    } catch (e) {
-      console.error(e);
-      console.error(JSON.stringify(e));
-    }
+      model.gameId(formattedId());
+    });
+  } catch (e) {
+    console.error(e);
+    console.error(JSON.stringify(e));
   }
-  copyReplayIDButton();
 }
+copyReplayIDButton();
